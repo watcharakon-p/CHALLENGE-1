11f189c679cd82194c773bab750c243b613c6b17
Watcharakon <watcharakorn@pixilla.co.th>
2025-09-16 17:32:31 +0700

    master

---
+++ api/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+    
